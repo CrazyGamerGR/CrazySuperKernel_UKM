2369326d34eaa48704ccd1fe0e45a8a61725fccc
FrancescoCG <frank-97@hotmail.gr>
2016-10-29 01:47:25 +0300

    fix full support for every s5 variant

--- data/UKM/device/.device.sh
+++ data/UKM/device/.device.sh
@@ -9,7 +9,7 @@ if [ -z "$DEVICE" ]; then DEVICE=`$BB grep "$DEVPROP=" /system/build.prop | $BB 
 
 #Unofficial
 case $DEVICE in
-	s5|klte|g900f|sm-g900f|kltevzw|SM-g900v|SM-G900I|SM-G900T|SM-G900P|SM-G900M|SM-G900W8|g900v|G900I|G900T|G900P|G900M|G900W8|samsung)
+	s5|klte|g900f|sm-g900f|kltevzw|klteduos|kltespr|klteusc|kltedv|SM-g900v|SM-G900I|SM-G900T|SM-G900P|SM-G900M|SM-G900W8|g900v|G900I|G900T|G900P|G900M|G900W8|samsung)
 		CONFIG="s5";; #SAMSUNG GALAXY S5
 	*)
 		CONFIG="";; #Not defined
